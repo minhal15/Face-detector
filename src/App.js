0a347154c8a821422f57195403331b4c4d139f44
minhal15 <64689808+minhal15@users.noreply.github.com>
2022-11-20 01:15:15 -0500

    files upload

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+import Navigation from './components/navigation/navigation.js';
+import Logo from './components/Logo/logo';
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
+import './App.css';
+
+const app = new Clarifai.App({
+  apiKey:'My key' // enter your API key here
+}) ;
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  };
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+
+
+    console.log('click');
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then((response)=>{
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      }) 
+      .catch((err) => {
+        console.log(err);
+      });    
+    
+
+  }
+
+  render() {
+    const { imageUrl, box } = this.state;
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition box={box} imageUrl={imageUrl}/>
+      </div>
+
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Logo/logo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import head from './head.png';
+import './logo.css';
+
+const Logo = () => {
+    return (
+        <div className="ma4 mt0">
+{/*             <Tilt className="Tilt br2 shadow-3" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+            <div className="Tilt-inner pa3"> 
+            <img syle={{paddingTop: '5px'}} alt= 'logo' src={head}/> </div>
+            </Tilt> */}
+        </div>
+    );
+}
+
+export default Logo;
