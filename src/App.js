b9b229ddc4f443c5a76d72de8c1814c6d17215fc
minhal15 <64689808+minhal15@users.noreply.github.com>
2023-07-09 02:13:23 -0400

    connected to db, only styling remains

--- src/App.js
+++ src/App.js
@@ -110,17 +110,17 @@ class App extends Component {
         .then(response => response.json())
         .then((response)=>{
           if (response) {
-            fetch('http://localhost:3000/imageurl', {
+            fetch('http://localhost:3000/image', {
               method: 'put',
               headers: {'Content-Type': 'application/json'},
               body: JSON.stringify({
                   id: this.state.user.id
                 })
             })
-            .then(response => response.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count }))
-            })
+              .then(response => response.json())
+              .then(count => {
+                this.setState(Object.assign(this.state.user, { entries: count }))
+              })
           }
             this.displayFaceBox(this.calculateFaceLocation(response));
           }) 
