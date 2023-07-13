1327d40bc34281299cae4428663294756ed04b15
minhal15 <64689808+minhal15@users.noreply.github.com>
2023-07-12 22:57:48 -0400

    edit

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -21,7 +21,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://face-detection-api-bjp0.onrender.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
